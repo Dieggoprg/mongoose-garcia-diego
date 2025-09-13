ce32cd856a495ca12d2530d1adf58f409fc37550
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 11:55:53 -0300

    Estructura básica

---
+++ app.js
@@ -0,0 +1,39 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./src/config/database.js"
+
+const PORT = process.env.PORT
+const app = express()
+
+// Estudiantes – Perfiles (1:1)
+
+// Profesores – Cursos (1:N)
+
+// Estudiantes – Cursos (N:M)
+
+// Cursos – Unidades (1:N)
+
+// Cursos – Evaluaciones (1:N)
+
+//middlewares
+app.use(express.json())
+app.use(cors({
+    // origin: 
+}))
+
+
+//rutas 
+
+
+//test conection BD
+connectDB()
+.then(() => {
+ app.listen(PORT, () => {
+   console.log("Server listening on port", PORT);
+  });
+})
+ .catch((error) => {
+ console.error("Error connecting to dataBase:", error);
+});
+
---
+++ src/models/coursesUnits.model.js
@@ -0,0 +1,1 @@
+//modelo Unidades de los cursos
