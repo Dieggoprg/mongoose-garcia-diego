e0180c9926b425407c6dfa7ca22b3f05331637e7
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 12:30:58 -0300

    se creó el modelo 'EVALUACIONES'

--- src/models/assessments.model.js
+++ src/models/assessments.model.js
@@ -1,1 +1,26 @@
 //modelo evaluaciones
+import {model , Schema} from "mongoose"
+
+const assessmentsSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        score: {
+            type: String,
+            required: true
+        },
+        dataTaken: {
+            type: Date,
+            default: "No definida"
+
+        },
+        passed: {
+            type: Boolean
+        }
+    }
+);
+
+export const AssessmentsModel = model("Assessments", assessmentsSchema)
