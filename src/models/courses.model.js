27d9b79385c70e4e7e777aacaad6cb2ac56bcb20
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 12:23:00 -0300

    se creó el modelo 'CURSOS'

--- src/models/courses.model.js
+++ src/models/courses.model.js
@@ -1,1 +1,18 @@
 //modelo cursos
+import {model , Schema} from "mongoose"
+
+const coursesSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        description: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+export const CoursesModel = model("Courses", coursesSchema)
