3ccbef9d027a2cda5e376b99d1a3c9135c533e23
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 12:11:40 -0300

    se creó el modelo 'ESTUDIANTES'

--- src/models/students.model.js
+++ src/models/students.model.js
@@ -1,1 +1,21 @@
 //modelo estudiantes
+import {model , Schema} from "mongoose"
+
+const studentsSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        }
+    }
+);
+
+export const StudentsModel = model("Teacher", studentsSchema)
