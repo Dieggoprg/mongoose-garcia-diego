5002ab586e959b402dee92f9cb315861a817642e
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 12:08:24 -0300

    se creó el modelo 'PROFESORES'

--- src/models/teachers.model.js
+++ src/models/teachers.model.js
@@ -1,1 +1,21 @@
 //modelo profesores
+import {model , Schema} from "mongoose"
+
+const teachersSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        }
+    }
+);
+
+export const TeachersModel = model("Teacher", teachersSchema)
