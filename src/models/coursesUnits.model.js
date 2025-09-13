3ff1a37fffebc39931a1971be6bc05e3273f2628
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 12:19:54 -0300

    se creó el modelo 'UNIDADES'

--- src/models/coursesUnits.model.js
+++ src/models/coursesUnits.model.js
@@ -1,1 +1,16 @@
 //modelo Unidades de los cursos
+import {model , Schema} from "mongoose"
+
+const coursesUnitsSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String
+        }
+    }
+);
+
+export const CoursesUnitsModel = model("Courses_Units", coursesUnitsSchema)
