f024521214c512802423accd1aa54ee70c7a38b9
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 12:15:24 -0300

    se creó el modelo 'PERFILES'

--- src/models/profiles.model.js
+++ src/models/profiles.model.js
@@ -1,1 +1,22 @@
 //modelo perfiles
+import {model , Schema} from "mongoose"
+
+const profilesSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: Number,
+            required: true,
+        }
+    }
+);
+
+export const ProfilesModel = model("Profiles", profilesSchema)
