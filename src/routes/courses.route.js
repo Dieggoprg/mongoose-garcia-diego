dd933af36e9359451ca0cb490bb6a2570538c946
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 22:22:14 -0300

    se creó el CRUD básico para el modelo 'Courses'

--- src/routes/courses.route.js
+++ src/routes/courses.route.js
@@ -1,1 +1,11 @@
 //routes cursos
+import { Router } from "express";
+import { createCourse, getAllCourses, updateCourse, getByIdCourse, deleteCourse } from "../controllers/courses.controller.js";
+
+export const routerCourse = Router(); 
+
+routerCourse.post("/postCourse", createCourse)
+routerCourse.get("/getAllCourses", getAllCourses)
+routerCourse.get("/getByIdCourse/:id", getByIdCourse)
+routerCourse.put("/updateCourse/:id", updateCourse)
+routerCourse.delete("/deleteCourse/:id", deleteCourse)
