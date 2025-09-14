04b61a02cf1c04be0a820726d94d627e62320db6
Diego Garcia <tu_correo_de_github@example.com>
2025-09-13 22:14:09 -0300

    se creó el CRUD básico para el modelo 'Courses_Units'

--- src/routes/coursesUnits.route.js
+++ src/routes/coursesUnits.route.js
@@ -1,1 +1,11 @@
 //routes Unidades
+import { Router } from "express";
+import { createCourseUnit, getAllCoursesUnits, updateCourseUnit, getByIdCourseUtit, deleteCourseUnit } from "../controllers/coursesUnits.controller.js";
+
+export const routerCourseUnit = Router(); 
+
+routerCourseUnit.post("/postCourseUnit", createCourseUnit)
+routerCourseUnit.get("/getAllCoursesUnits", getAllCoursesUnits)
+routerCourseUnit.get("/getByIdCourseUnit/:id", getByIdCourseUtit)
+routerCourseUnit.put("/updateCourseUnit/:id", updateCourseUnit)
+routerCourseUnit.delete("/deleteCourseUnit/:id", deleteCourseUnit)
